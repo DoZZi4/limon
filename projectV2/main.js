dcec5b22cfa51a38353abe2f82d22c35058d6d42
DoZZi4 <erog98@icloud.com>
2022-06-23 12:00:40 +0300

    change

---
+++ projectV2/main.js
@@ -0,0 +1,36 @@
+const express = require("express");
+// let HTTP = require("http");
+// let requestListener = function(req,res){
+    
+//     if (req.url==="/"){
+//         res.writeHead(200)
+//         res.end("Если ты это читаешь ты красавчик")
+//     }
+
+//     if (req.url==="/date"){
+//         res.writeHead(200)
+//         res.end(new Date())
+//     }
+
+//     if(req.url==="/info"){
+//         res.writeHead(200)
+//         res.end(JSON.stringify({user: "Jhon",
+//         email: "test@mail.ru"}))
+//     }
+// }   
+//     let server = HTTP.createServer(requestListener)
+//     server.listen(5000)
+    const app = express()
+    app.get("/",function(req,res){
+        res.send("hello broyk")
+    })
+    app.get("/date", function(req,res){
+        res.send(new Date())
+    })
+    app.get("/info", function(req,res){
+        res.send(JSON.stringify({user: "Jhon",
+            email: "test@mail.ru"}))
+    })
+        
+    
+    app.listen(7070)
