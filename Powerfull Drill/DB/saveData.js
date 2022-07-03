f257fb242d491ade2177fda9d94e918989eec7cf
Kletskov Gleb <gfgfdgleb@gmail.com>
2022-07-03 10:25:06 +0300

    Update task

--- Powerfull Drill/DB/saveData.js
+++ Powerfull Drill/DB/saveData.js
@@ -1,2 +1,11 @@
+// saveData должна сохранять переданные данные в data.json
+// Логика работы
+// 1 - читаем данные с помощью getData, не забыть про JSON.parse
+// 2 - Заменяем данные на данные из аргумента функции 
+// 3 - Пишем данные в data.json, не забыть про JSON.stringify
+// Ссылка на запись данных 
+// https://www.geeksforgeeks.org/node-js-fs-writefilesync-method/
+// https://nodejs.dev/learn/writing-files-with-nodejs
+
 const saveData = ()=>{}
 module.exports = saveData
